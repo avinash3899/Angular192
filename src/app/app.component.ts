e87b7491d795bd36135044791996328cac4dc0ec
Avinash Sinha <avinash3899@gmail.com>
2021-08-12 15:19:37 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<form>
+  <table>
+  <tr><td><label>Inventory ID:</label><td>
+  <input type="text" name="pid" size=20 /><br /></tr>
+  <tr><td><label>Inventory Name:</label><td>
+  <input type="text" name="pname" size=20 /><br /></tr>
+  <tr><td><label>Inventory Unit:</label><td>
+  <input type="text" name="punit" size=20 /><br /></tr>
+  <tr><td><label>Inventory Price:</label><td>
+  <input type="text" name="pprice" size=20 /><br /></tr>
+  <tr><td><td><input type="submit" value="submit" /><br /></tr>
+  </table>
+  </form>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
